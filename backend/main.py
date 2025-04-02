cdce9592304a4aadde661511b28e63d48b27669e
Irfan Prasetyo <irfan@catalyzecommunications.com>
2025-04-02 21:09:46 +0700

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,20 @@ import json
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Load dummy data
-with open("dummyData.json", "r") as f:
+with open("../dummyData.json", "r") as f:
     DUMMY_DATA = json.load(f)
 
 @app.get("/api/data")
